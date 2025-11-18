0cfe5d551090d9d8f0d23c75346e1d0abccd5e69
Kevin Teynor <kevinteynor@gmail.com>
2025-11-18 09:26:07 -0500

    update nixos to 25.05. use plasma6 instead of plasma5.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 NixOS System Configuration
 
+### system upgrade steps:
+
+1. update `nixpkgs.url` value in flake.nix `inputs`
+2. run `nix flake update`
+3. run `nixos-rebuild switch --upgrade --flake .`
+
--- configuration.nix
+++ configuration.nix
@@ -57,7 +57,8 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
