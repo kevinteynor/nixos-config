9cf971b7909888fecd88e201c1ca0955ca7d5d71
Kevin Teynor <kevinteynor@gmail.com>
2026-04-12 10:38:29 -0400

    add ghostty, steam.

--- configuration.nix
+++ configuration.nix
@@ -116,6 +116,7 @@
     git
     wget
     curl
+    ghostty
 
     # editors
     
@@ -130,6 +131,7 @@
   ];
 
   programs.fish.enable = true;
+  programs.steam.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
